bde35e9f1c3b8ea2415c99890fe48297f8bd33d0
Kristina Antoniuk <kristina.antoniuk@ebcont.com>
2024-02-15 14:46:35 +0100

    Routes configured

--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,9 +1,12 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './shared/components/errors/not-found/not-found.component';
+import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
+    {path: 'account', loadChildren: () => import('./account/account.routes').then(r => r.ACCOUNT_ROUTES)},
+    {path: 'test', component: NotFoundComponent},
     {path: 'not-found', component: NotFoundComponent},
     {path: '**', component: NotFoundComponent, pathMatch: 'full'}
 ];
