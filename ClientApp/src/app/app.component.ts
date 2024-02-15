a446b9efe8617b82f2937b0bdeb862bf610d53d1
Kristina Antoniuk <kristina.antoniuk@ebcont.com>
2024-02-15 16:04:41 +0100

    RouterLinks are fixed.

--- ClientApp/src/app/app.component.ts
+++ ClientApp/src/app/app.component.ts
@@ -1,12 +1,14 @@
 import { Component } from '@angular/core';
-import { RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './navbar/navbar.component';
 import { FooterComponent } from './footer/footer.component';
+import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, RouterLink, RouterLinkActive],
+  imports: [RouterOutlet,
+            NavbarComponent, 
+            FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
--- ClientApp/src/app/app.routes.ts
+++ ClientApp/src/app/app.routes.ts
@@ -1,7 +1,6 @@
 import { Routes } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 import { NotFoundComponent } from './shared/components/errors/not-found/not-found.component';
-import { AppComponent } from './app.component';
 
 export const routes: Routes = [
     {path: '', component: HomeComponent},
