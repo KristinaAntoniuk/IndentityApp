27868cbd61104350fafba1e359ef703294cb95ca
Kristina Antoniuk <kristina_antoniuk@outlook.com>
2024-02-23 16:58:41 +0100

    Login behavior implementation started

--- ClientApp/src/app/navbar/navbar.component.ts
+++ ClientApp/src/app/navbar/navbar.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
+import { AccountService } from '../account/account.service';
+import { CommonModule, NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterModule],
+  imports: [RouterModule, NgIf, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
 export class NavbarComponent {
-
+    constructor(public accountService: AccountService) {}
 }
