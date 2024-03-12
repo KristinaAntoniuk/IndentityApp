35e0da9cc3068d9ab82b2484b1873ccd66547a7b
Kristina Antoniuk <kristina_antoniuk@outlook.com>
2024-03-12 17:20:38 +0100

    SMTP Mail Service option is added.

--- Services/EmailService.cs
+++ Services/EmailService.cs
@@ -2,6 +2,9 @@
 using Mailjet.Client;
 using Mailjet.Client.TransactionalEmails;
 using Microsoft.Extensions.Configuration;
+using System.Net.Mail;
+using System.Net;
+using System;
 using System.Threading.Tasks;
 
 namespace IndentityApp.Services
@@ -37,6 +40,28 @@ namespace IndentityApp.Services
             }
 
             return false;
+
+            #region SMTP Mail Service
+            //try
+            //{
+            //    var client = new SmtpClient("smtp-mail.outlook.com", 587)
+            //    {
+            //        EnableSsl = true,
+            //        Credentials = new NetworkCredential(_config["SMTP:Username"], _config["SMTP:Password"])
+            //    };
+
+            //    var message = new MailMessage(from: _config["SMTP:Username"],
+            //        to: emailSend.To, subject: emailSend.Subject, body: emailSend.Body);
+
+            //    message.IsBodyHtml = true;
+            //    await client.SendMailAsync(message);
+            //    return true;
+            //}
+            //catch (Exception)
+            //{
+            //    return false;
+            //}
+            #endregion
         }
     }
 }
